c58a921b5c57ebcde02451335746cd68c62f9fe5
Dmitry Mozgin <m0391n@gmail.com>
2017-02-15 13:46:51 +0300

    Empty AndroidXLExample

---
+++ Cpp-Cross-Platform/CSharpProjects/AndroidXLExample/MainActivity.cs
@@ -0,0 +1,22 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace AndroidXLExample
+{
+	[Activity(Label = "AndroidXL", MainLauncher = true, Icon = "@mipmap/icon")]
+	public class MainActivity : Activity
+	{
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+
+			// Set our view from the "main" layout resource
+			SetContentView(Resource.Layout.Main);
+
+			FindViewById<TextView>(Resource.Id.main_label)
+				.Text = "Hello,\nm039!";
+		}
+	}
+}
+
