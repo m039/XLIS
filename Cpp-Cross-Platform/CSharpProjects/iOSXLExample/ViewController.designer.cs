984b4af23be67459a5db121cf291329e13d47803
Dmitry Mozgin <m0391n@gmail.com>
2017-02-15 13:39:17 +0300

    Empty iOSXLExample

---
+++ Cpp-Cross-Platform/CSharpProjects/iOSXLExample/ViewController.designer.cs
@@ -0,0 +1,26 @@
+// WARNING
+//
+// This file has been generated automatically by Visual Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
+//
+using Foundation;
+using System.CodeDom.Compiler;
+
+namespace iOSXLExample
+{
+	[Register ("ViewController")]
+	partial class ViewController
+	{
+		[Outlet]
+		UIKit.UILabel MainLabel { get; set; }
+		
+		void ReleaseDesignerOutlets ()
+		{
+			if (MainLabel != null) {
+				MainLabel.Dispose ();
+				MainLabel = null;
+			}
+		}
+	}
+}
